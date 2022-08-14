424955f1ffa92c9280fb5e700bcac82175228a63
mayankprg <mayankrawatprg@gmail.com>
2022-08-15 02:43:02 +0530

    add facivon

--- commerce/urls.py
+++ commerce/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+from django.views.generic.base import RedirectView
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("auctions.urls"))
+    path("", include("auctions.urls")),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('favicon/favicon.ico')))
 ]
